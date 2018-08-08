aedde20c0d9c92ab618024a4d2a5396ce10af527
Samuel Kerrien <samuel.kerrien@epfl.ch>
2018-08-08 22:46:06 +0200

    search: construction of the query from CLI params. So far POST request fails.

--- search/commands.py
+++ search/commands.py
@@ -1,6 +1,9 @@
 import click
 import os
+import requests
+import json
 from blessings import Terminal
+from http.client import responses
 
 import config_utils
 import nexus_utils
@@ -11,22 +14,58 @@ t = Terminal()
 
 
 @click.command()
-@click.option('--file',   help='name of the file to updload.')
-@click.option('--org',    help='The organisation in which to store that file.')
-@click.option('--domain', help='The domain in which to store that file.')
-@click.option('--type',   default='nxv:Entity', help='The type of the entity to be uploaded.')
-def upload(file, org, domain, type):
-    """Upload a file in Nexus."""
-    utils.error("upload not implemented yet")
+@click.option('--entity-type', '-t', help='The entity type to search for.')
+@click.option('--field', '-f', help='The field to filter on.')
+@click.option('--value', '-v', help='The field value to filter on.')
+@click.option('--show-query', '-s', is_flag=True, help='print the generated search query')
+@click.option('--pretty', '-p', is_flag=True, help='colorize output')
+def search(entity_type, field, value, show_query, pretty):
+    """Search Nexus."""
 
-    """Upload a given file into Nexus"""
-    if file is None:
-        utils.error('ERROR: you must give a filename')
+    query = {
+              "@context": "https://bbp.epfl.ch/nexus/v0/contexts/neurosciencegraph/core/data/v1.0.3",
+              "filter": {
+                "op": "and",
+                "value": []
+              }
+            }
 
-    print(os.path.abspath(file))
-    if not os.path.isfile(file):
-        utils.error('ERROR: File doesn''t exist:' + file)
-    else:
-        utils.success('File found.')
+    if entity_type is not None:
+        query['filter']['value'].append({
+                                            "op": "eq",
+                                            "path": "rdf:type",
+                                            "value": entity_type
+                                          })
 
-        # TODO perform upload into Nexus
+    if field is not None and value is None:
+        utils.error("if you provide a field, you must give a value")
+
+    if value is not None and field is None:
+        utils.error("if you provide a value, you must give a field")
+
+    if field is not None:
+        query['filter']['value'].append({
+            "op": "eq",
+            "path": field,
+            "value": value
+        })
+
+    if show_query:
+        utils.print_json(query, colorize=pretty)
+
+    # execute query
+
+    search_url = config_utils.get_selected_deployment_config()[1]['url'] + "/v0/queries"
+    headers = {"Content-Type": "application/json"}
+    access_token = config_utils.get_access_token()
+    if access_token is not None:
+        headers["Authorization"] = "Bearer " + access_token
+
+    r = requests.post(search_url, data=query, headers=headers)
+    if r.status_code != 200:
+        utils.error("Failed to search from URL: " + search_url +
+                    '\nRequest status: ' + str(r.status_code) + ' (' + responses[r.status_code] + ')')
+
+    # TODO print results
+
+
