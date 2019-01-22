d47d7105385349a3c6a501ccdbd5d7cb7a44c30b
Samuel Kerrien <samuel.kerrien@epfl.ch>
2019-01-22 12:40:38 +0100

    projects command completed. minor bugfix on profiles.

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ A command line interface for Nexus
 
 # Setup
 
-As seen in http://click.pocoo.org/6/setuptools/#setuptools-integration
+As seen in http://click.pocoo.org/7/setuptools/#setuptools-integration
 ```
    <install conda - https://conda.io/docs/user-guide/install/index.html>
 
-   conda create -y --name nexus-cli python=3.5
+   conda create -n nexus-cli python=3.5
    conda activate nexus-cli
    git clone https://github.com/BlueBrain/nexus-cli
    cd nexus-cli
@@ -15,7 +15,14 @@ As seen in http://click.pocoo.org/6/setuptools/#setuptools-integration
    pip install --editable .
 ```
 
-start using the CLI:
+or with straight from github:
+```
+    conda create -n nexus-cli python=3.5
+    conda activate nexus-cli
+    pip install git+https://github.com/BlueBrain/nexus-cli@nexus_v1
+```
+
+Start using the CLI:
 ```
     nexus --help
 ```
@@ -23,15 +30,31 @@ start using the CLI:
 # Currently supported features
 
 ## profiles
-* adding a new profile (--create <name> --url <URL>)
-* Deleting a locally registered deployment (--delete <name>)
-* list locally registered deployment (--list), 
-* select a locally registered deployment (--select <name>)
+* create: adding a new profile (create <name> <URL>)
+* delete: deleting a locally registered profile (delete <name>)
+* list: list locally registered profiles (list), 
+* select: select a locally registered profile (select <name>)
+* current: show currently selected profile
 
-## login
-* log into the currently selected local deployment of nexus
-* show token upon login with expiry status
+## auth
+* login: opens Nexus Web in your browser so that the user can login and get his/her token
+* set-token: saves a token in the currently selected profile
+* view-token: prints token from the currently selected profile (encoded, decoded, expiry time)
 
-## token (stored in ${HOME}/.nexus-cli/config.json)
-* print encoded token
-* print decoded token and expiry information
+## orgs
+* list: list all organizations
+* create: create a new organization
+* fetch: shows the json payload of an organization
+* update: update an organization (name and description only!)
+* deprecate: deprecate an organization
+* select: select the organization to use in subsequent calls of the CLI (local to the currently selected profile)
+* current: shows the currently selected organization
+
+## projects
+* list: list all projects
+* create: create a new project
+* fetch: shows the json payload of an project
+* update: update an project (name and description only!)
+* deprecate: deprecate an project
+* select: select the project to use in subsequent calls of the CLI (local to the currently selected profile)
+* current: shows the currently selected project
--- projects.py
+++ projects.py
@@ -1,8 +1,12 @@
 import click
-import utils
+from prettytable import PrettyTable
+import os, tempfile
+import json
+from collections import OrderedDict
+import hashlib
 
-
-from cli import cli
+from nexuscli import utils
+from nexuscli.cli import cli
 
 
 @cli.group()
@@ -10,41 +14,214 @@ def projects():
     """Projects operations"""
 
 
+def get_organization_label(given_org_label):
+    if given_org_label is None:
+        given_org_label = utils.get_default_organization()
+        if given_org_label is None:
+            utils.error("No organization specified, either set default using the 'orgs' command or pass it as a "
+                        "parameter using --org")
+    return given_org_label
+
+
+@projects.command(name='fetch', help='Fetch a project')
+@click.argument('label')
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+@click.option('--revision', '-r', default=None, type=int, help='Fetch the project at a specific revision')
+@click.option('--pretty', '-p', is_flag=True, default=False, help='Colorize JSON output')
+def fetch(label, _org_label, revision, pretty):
+    _org_label = get_organization_label(_org_label)
+    try:
+        nxs = utils.get_nexus_client()
+        response = nxs.projects.fetch(org_label=_org_label, project_label=label, rev=revision)
+        if revision is not None and response["_rev"] != revision:
+            utils.error("Revision '%s' does not exist" % revision)
+        utils.print_json(response, colorize=pretty)
+    except nxs.HTTPError as e:
+        utils.print_json(e.response.json(), colorize=True)
+        utils.error(str(e))
+
+
 @projects.command(name='create', help='Create a new project')
-@click.argument('name')
-@click.option('--org', '-o', help='Organization to work on (overrides selection made with --orgs)')
-def create(name, org):
-    utils.error("Not implemented yet")
+@click.argument('label')
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+@click.option('--name', '-n', help='The name of this project')
+@click.option('--base', '-b', help='The base of this project')
+@click.option('--vocab', '-v', help='The vocab of this project')
+@click.option('--prefix', '-p', multiple=True, help='Prefix mapping, can be used multiple times '
+                                                    '(format: <prefix>=<namespace>)')
+@click.option('_json', '--json-only', '-j', is_flag=True, default=False, help='Print JSON payload returned by the '
+                                                                              'nexus API')
+@click.option('--pretty', '-p', is_flag=True, default=False, help='Colorize JSON output')
+def create(label, _org_label, name, base, vocab, prefix, _json, pretty):
+    _org_label = get_organization_label(_org_label)
+    try:
+        config = {}
+        if name is not None:
+            config["name"] = name
+
+        if base is not None:
+            config["base"] = base
+
+        if vocab is not None:
+            config["vocab"] = vocab
+
+        if prefix is not None:
+            config["apiMappings"] = []
+            for p in prefix:
+                if "=" not in p:
+                    utils.error("Invalid prefix mapping, it should be in the format <prefix>=<URL>: %s" % p)
+                key, value = p.split("=", 1)
+                entry = {
+                    "prefix": key,
+                    "namespace": value
+                }
+                config["apiMappings"].append(entry)
+        nxs = utils.get_nexus_client()
+        response = nxs.projects.create(org_label=_org_label, project_label=label, config=config)
+        print("Project created (id: %s)" % response["@id"])
+        if _json:
+            utils.print_json(response, colorize=pretty)
+    except nxs.HTTPError as e:
+        utils.print_json(e.response.json(), colorize=True)
+        utils.error(str(e))
+
+
+@projects.command(name='fetch', help='Fetch a project')
+@click.argument('label')
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+@click.option('--revision', '-r', default=None, type=int, help='Fetch the project at a specific revision')
+@click.option('--pretty', '-p', is_flag=True, default=False, help='Colorize JSON output')
+def fetch(label, _org_label, revision, pretty):
+    _org_label = get_organization_label(_org_label)
+    try:
+        nxs = utils.get_nexus_client()
+        response = nxs.projects.fetch(org_label=_org_label, project_label=label, rev=revision)
+        if revision is not None and response["_rev"] != revision:
+            utils.error("Revision '%s' does not exist" % revision)
+        utils.print_json(response, colorize=pretty)
+    except nxs.HTTPError as e:
+        utils.print_json(e.response.json(), colorize=True)
+        utils.error(str(e))
 
 
 @projects.command(name='update', help='Update a project')
-@click.argument('id')
-@click.option('--org', '-o', help='Organization to work on (overrides selection made with --orgs)')
-@click.option('--data', '-d', help='Payload to replace it with')
-def update(id, org, data):
-    utils.error("Not implemented yet")
+@click.argument('label')
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+@click.option('_payload', '--data', '-d', help='Payload to replace it with')
+def update(label, _org_label, _payload):
+    _org_label = get_organization_label(_org_label)
+    try:
+        nxs = utils.get_nexus_client()
+        data = nxs.projects.fetch(org_label=_org_label, project_label=label)
+        data_ordered = OrderedDict(sorted(data.items()))
+        data_md5_before = hashlib.md5(json.dumps(data_ordered, indent=2).encode('utf-8')).hexdigest()
+        current_revision = data["_rev"]
+
+        if _payload is not None:
+            data = json.loads(_payload)
+        else:
+            # If no payload given, load up the entry in a text file and open default editor
+            new_file, filename = tempfile.mkstemp()
+            print("Opening an editor: %s" % filename)
+            f = open(filename, "w")
+            f.write(json.dumps(data, indent=2))
+            f.close()
+            click.edit(filename=filename)
+            f = open(filename, "r")
+            data = json.loads(f.read())
+            f.close()
+            os.remove(filename)
+
+        data_ordered = OrderedDict(sorted(data.items()))
+        data_md5_after = hashlib.md5(json.dumps(data_ordered, indent=2).encode('utf-8')).hexdigest()
+        if data_md5_before == data_md5_after:
+            print("No change in project, aborting update.")
+        else:
+            nxs.projects.update(project=data, previous_rev=current_revision)
+            print("Project updated.")
+    except nxs.HTTPError as e:
+        utils.print_json(e.response.json(), colorize=True)
+        utils.error(str(e))
 
 
 @projects.command(name='list', help='List all projects')
-@click.option('--org', '-o', help='Organization to work on (overrides selection made with --orgs)')
-def _list(org):
-    utils.error("Not implemented yet")
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+@click.option('_json', '--json-only', '-j', is_flag=True, default=False, help='Print JSON payload returned by the nexus API')
+@click.option('--pretty', '-p', is_flag=True, default=False, help='Colorize JSON output')
+def _list(_org_label, _json, pretty):
+    _org_label = get_organization_label(_org_label)
+    try:
+        nxs = utils.get_nexus_client()
+        response = nxs.projects.list(org_label=_org_label)
+        if _json:
+            utils.print_json(response, colorize=pretty)
+        else:
+            table = PrettyTable(['Name', 'Description', 'Id', 'Deprecated'])
+            table.align["Name"] = "l"
+            table.align["Description"] = "l"
+            table.align["Id"] = "l"
+            table.align["Deprecated"] = "l"
+            for r in response["_results"]:
+                if "description" in r:
+                    table.add_row([r["_label"], r["description"], r["@id"], r["_deprecated"]])
+                else:
+                    table.add_row([r["_label"], "", r["@id"], r["_deprecated"]])
+            print(table)
+    except nxs.HTTPError as e:
+        utils.print_json(e.response.json(), colorize=True)
+        utils.error(str(e))
 
 
 @projects.command(name='deprecate', help='Deprecate an project')
-@click.argument('name')
-@click.option('--org', '-o', help='Organization to work on (overrides selection made with --orgs)')
-def deprecate(name, org):
-    utils.error("Not implemented yet")
+@click.argument('label')
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+@click.option('_json', '--json-only', '-j', is_flag=True, default=False, help='Print JSON payload returned by the nexus API')
+@click.option('--pretty', '-p', is_flag=True, default=False, help='Colorize JSON output')
+def deprecate(label, _org_label, _json, pretty):
+    _org_label = get_organization_label(_org_label)
+    try:
+        nxs = utils.get_nexus_client()
+        response = nxs.projects.fetch(org_label=_org_label, project_label=label)
+        if _json:
+            utils.print_json(response, colorize=pretty)
+        response = nxs.projects.deprecate(org_label=_org_label, project_label=label, previous_rev=response["_rev"])
+        if _json:
+            utils.print_json(response, colorize=pretty)
+        print("Project '%s' under organization '%s' was deprecated." % (label, _org_label))
+    except nxs.HTTPError as e:
+        utils.print_json(e.response.json(), colorize=True)
+        utils.error(str(e))
 
 
 @projects.command(name='select', help='Select an project')
-@click.argument('name')
-@click.option('--org', '-o', help='Organization to work on (overrides selection made with --orgs)')
-def select(name, org):
-    utils.error("Not implemented yet")
+@click.argument('label')
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+def select(label, _org_label):
+    _org_label = get_organization_label(_org_label)
+    try:
+        nxs = utils.get_nexus_client()
+        nxs.projects.fetch(org_label=_org_label, project_label=label)
+    except nxs.HTTPError as e:
+        if e.response.status_code == 404:
+            utils.error("Could not find project with label '%s' under organization '%s'." % (label, _org_label))
+        else:
+            # unexpected error
+            utils.print_json(e.response.json(), colorize=True)
+            utils.error(str(e))
+
+    utils.set_default_project(label)
+    print("Project selected.")
 
 
 @projects.command(name='current', help='Show currently selected project')
 def current():
-    utils.error("Not implemented yet")
+    config = utils.get_cli_config()
+    profile, selected_config = utils.get_selected_deployment_config(config)
+    if selected_config is None:
+        utils.error("You must first select a profile using the 'profiles' command")
+
+    default_project = utils.get_default_project()
+    if default_project is not None:
+        print(default_project)
+    else:
+        utils.error("No default project selected in profile '%s'" % profile)
