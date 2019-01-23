144f58376c15a48c6836c49a4e509afc3ead9c09
Samuel Kerrien <samuel.kerrien@epfl.ch>
2019-01-23 17:08:18 +0100

    New command: resources + minor improvements

--- nexuscli/resources.py
+++ nexuscli/resources.py
@@ -1,7 +1,13 @@
+import collections
+
 import click
-from nexuscli import utils
+from prettytable import PrettyTable
+import json
+import os
+import tempfile
 
 from nexuscli.cli import cli
+from nexuscli import utils
 
 
 @cli.group()
@@ -10,43 +16,176 @@ def resources():
 
 
 @resources.command(name='create', help='Create a new resource')
-@click.argument('name')
-@click.option('--org', '-o', help='Organization to work on (overrides selection made with --orgs)')
-@click.option('--project', '-p', help='Project to work on (overrides selection made with --projects)')
-@click.option('--id', '-f', help='Id of the resource')
-@click.option('--file', '-f', help='source file to create new resource')
-@click.option('--data', '-d', help='source payload to create new resource')
-def create(name, org, project, id, file, data):
-    utils.error("Not implemented yet")
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+@click.option('_prj_label', '--project', '-p', help='Project to work on (overrides selection made via projects command)')
+@click.option('--id', '-i', help='Id of the resource')
+@click.option('_payload', '--data', '-d', help='source payload to create new resource')
+@click.option('--schema', '-s', default=None, help='Schema to validate this resource against')
+@click.option('_json', '--json', '-j', is_flag=True, default=False, help='Print JSON payload returned by the nexus API')
+@click.option('--pretty', is_flag=True, default=False, help='Colorize JSON output')
+def create(_org_label, _prj_label, id, file, _payload, schema, _json, pretty):
+    _org_label = utils.get_organization_label(_org_label)
+    _prj_label = utils.get_project_label(_prj_label)
+    nxs = utils.get_nexus_client()
+    try:
+        data = {}
+        if _payload is not None:
+            data = json.loads(_payload)
+        if id is not None:
+            data['@id'] = id
+        response = nxs.resources.create(org_label=_org_label, project_label=_prj_label, data=data,
+                                        schema_id=schema, id=id)
+        print("Resource created (id: %s)" % response["@id"])
+        if _json:
+            utils.print_json(response, colorize=pretty)
+    except nxs.HTTPError as e:
+        utils.print_json(e.response.json(), colorize=True)
+        utils.error(str(e))
+
+
+@resources.command(name='fetch', help='Fetch a resource')
+@click.argument('id')
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+@click.option('_prj_label', '--project', '-p', help='Project to work on (overrides selection made via projects command)')
+@click.option('--schema', '-s', default='_', help='Schema of the resource')
+@click.option('--revision', '-r', default=None, type=int, help='Fetch the resource at a specific revision')
+@click.option('--tag', '-t', default=None, help='Fetch the resource at a specific tag')
+@click.option('--pretty', is_flag=True, default=False, help='Colorize JSON output')
+def fetch(id, _org_label, _prj_label, schema, revision, tag, pretty):
+    _org_label = utils.get_organization_label(_org_label)
+    _prj_label = utils.get_project_label(_prj_label)
+    nxs = utils.get_nexus_client()
+    try:
+        response = nxs.resources.fetch(org_label=_org_label, project_label=_prj_label, schema_id=schema,
+                                       resource_id=id, rev=revision, tag=tag)
+        utils.print_json(response, colorize=pretty)
+    except nxs.HTTPError as e:
+        utils.print_json(e.response.json(), colorize=True)
+        utils.error(str(e))
 
 
 @resources.command(name='update', help='Update a resource')
 @click.argument('id')
-@click.option('--org', '-o', help='Organization to work on (overrides selection made with --orgs)')
-@click.option('--project', '-p', help='Project to work on (overrides selection made with --projects)')
-@click.option('--data', '-d', help='Payload to replace it with')
-def update(id, org, project, data):
-    utils.error("Not implemented yet")
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+@click.option('_prj_label', '--project', '-p', help='Project to work on (overrides selection made via projects command)')
+@click.option('_payload', '--data', '-d', help='Payload to replace it with')
+def update(id, _org_label, _prj_label, _payload):
+    _org_label = utils.get_organization_label(_org_label)
+    _prj_label = utils.get_project_label(_prj_label)
+    nxs = utils.get_nexus_client()
+    try:
+        data = nxs.resources.fetch(org_label=_org_label, project_label=_prj_label, schema_id='_', resource_id=id)
+        data_md5_before = utils.generate_nexus_payload_checksum(data)
+        current_revision = data["_rev"]
+
+        if _payload is not None:
+            data = json.loads(_payload)
+        else:
+            # If no payload given, load up the entry in a text file and open default editor
+            new_file, filename = tempfile.mkstemp()
+            print("Opening an editor: %s" % filename)
+            f = open(filename, "w")
+            f.write(json.dumps(data, indent=2))
+            f.close()
+            click.edit(filename=filename)
+            f = open(filename, "r")
+            data = json.loads(f.read())
+            f.close()
+            os.remove(filename)
+
+        data_md5_after = utils.generate_nexus_payload_checksum(data)
+        if data_md5_before == data_md5_after:
+            print("No change in resource, aborting update.")
+        else:
+            nxs.resources.update(resource=data, previous_rev=current_revision)
+            print("Resource updated.")
+    except nxs.HTTPError as e:
+        utils.print_json(e.response.json(), colorize=True)
+        utils.error(str(e))
 
 
 @resources.command(name='list', help='List all resources')
-@click.option('--org', '-o', help='Organization to work on (overrides selection made with --orgs)')
-@click.option('--project', '-p', help='Project to work on (overrides selection made with --projects)')
-def _list(org, project):
-    utils.error("Not implemented yet")
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+@click.option('_prj_label', '--project', '-p', help='Project to work on (overrides selection made via projects command)')
+@click.option('--deprecated', '-d', is_flag=True, default=False, help='Show only deprecated resources')
+@click.option('_from', '--from', '-f', default=0, help='Offset of the listing')
+@click.option('--size', '-s', default=20, help='How many resource to list')
+@click.option('--search', default=None, help='Full text search on the resources')
+@click.option('_json', '--json', '-j', is_flag=True, default=False, help='Print JSON payload returned by the nexus API')
+@click.option('--pretty', is_flag=True, default=False, help='Colorize JSON output')
+def _list(_org_label, _prj_label, deprecated, _from, size, search, _json, pretty):
+    _org_label = utils.get_organization_label(_org_label)
+    _prj_label = utils.get_project_label(_prj_label)
+    nxs = utils.get_nexus_client()
+    try:
+        response = nxs.resources.list(org_label=_org_label, project_label=_prj_label,
+                                      pagination_from=_from, pagination_size=size,
+                                      deprecated = deprecated, full_text_search_query=search)
+        if _json:
+            utils.print_json(response, colorize=pretty)
+        else:
+            table = PrettyTable(['Id', 'Type', 'Revision', 'Deprecated'])
+            table.align["Id"] = "l"
+            table.align["Type"] = "l"
+            table.align["Revision"] = "l"
+            table.align["Deprecated"] = "l"
+            for r in response["_results"]:
+                types = ""
+                if "@type" in r:
+                    if type(r["@type"]) is str:
+                        types = r["@type"]
+                    elif isinstance(r["@type"], collections.Sequence):
+                        for t in r["@type"]:
+                            types += t + "\n"
+                    else:
+                        utils.warn("Unsupported type: " + type(r["@type"]))
+                        types = r["@type"]
+                else:
+                    types = '-'
+
+                table.add_row([r["@id"], types, r["_rev"], r["_deprecated"]])
+            print(table)
+    except nxs.HTTPError as e:
+        utils.print_json(e.response.json(), colorize=True)
+        utils.error(str(e))
 
 
 @resources.command(name='deprecate', help='Deprecate an resource')
 @click.argument('id')
-@click.option('--org', '-o', help='Organization to work on (overrides selection made with --orgs)')
-@click.option('--project', '-p', help='Project to work on (overrides selection made with --projects)')
-def deprecate(id, org, project):
-    utils.error("Not implemented yet")
+@click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+@click.option('_prj_label', '--project', '-p', help='Project to work on (overrides selection made via projects command)')
+@click.option('_json', '--json', '-j', is_flag=True, default=False, help='Print JSON payload returned by the nexus API')
+@click.option('--pretty', is_flag=True, default=False, help='Colorize JSON output')
+def deprecate(id, _org_label, _prj_label, _json, pretty):
+    _org_label = utils.get_organization_label(_org_label)
+    _prj_label = utils.get_project_label(_prj_label)
+    nxs = utils.get_nexus_client()
+    try:
+        response = nxs.resources.fetch(org_label=_org_label, project_label=_prj_label, resource_id=id)
+        if _json:
+            utils.print_json(response, colorize=pretty)
+        response = nxs.resources.deprecate(org_label=_org_label, project_label=_prj_label, resource=response,
+                                           previous_rev=response["_rev"])
+        if _json:
+            utils.print_json(response, colorize=pretty)
+        print("Resource '%s' was deprecated." % (id))
+    except nxs.HTTPError as e:
+        utils.print_json(e.response.json(), colorize=True)
+        utils.error(str(e))
 
-
-@resources.command(name='fetch', help='Get payload of a resource by id')
-@click.argument('id')
-@click.option('--org', '-o', help='Organization to work on (overrides selection made with --orgs)')
-@click.option('--project', '-p', help='Project to work on (overrides selection made with --projects)')
-def fetch(id, org, project):
-    utils.error("Not implemented yet")
+#
+# @resources.command(name='fetch', help='Get payload of a resource by id')
+# @click.argument('id')
+# @click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
+# @click.option('_prj_label', '--project', '-p', help='Project to work on (overrides selection made via projects command)')
+# @click.option('--pretty', is_flag=True, default=False, help='Colorize JSON output')
+# def fetch(id, _org_label, _prj_label, pretty):
+#     _org_label = utils.get_organization_label(_org_label)
+#     _prj_label = utils.get_project_label(_prj_label)
+#     nxs = utils.get_nexus_client()
+#     try:
+#         response = nxs.resources.fetch(org_label=_org_label, project_label=_prj_label, resource_id=id, schema_id='_')
+#         utils.print_json(response, colorize=pretty)
+#     except nxs.HTTPError as e:
+#         utils.print_json(e.response.json(), colorize=True)
+#         utils.error(str(e))
--- nexuscli/utils.py
+++ nexuscli/utils.py
@@ -1,29 +1,38 @@
-import os
-import sys
 from blessings import Terminal
 import json
 from pygments import highlight
 from pygments.lexers import JsonLdLexer
-from pygments.formatters import TerminalFormatter, TerminalTrueColorFormatter
+from pygments.formatters import TerminalFormatter
 import time
 from datetime import datetime
 from pathlib import Path
+import hashlib
+import os
+import sys
+from collections import OrderedDict
 
 import nexussdk as nxs
 
+from nexuscli.config import _DEFAULT_ORGANISATION_KEY_, _DEFAULT_PROJECT_KEY_, _URL_KEY_, _TOKEN_KEY_, _SELECTED_KEY_
 
-def error(message):
+
+def error(message: str):
     t = Terminal()
     print(t.red(message))
     sys.exit(101)
 
 
-def success(message):
+def warn(message: str):
+    t = Terminal()
+    print(t.yellow(message))
+
+
+def success(message: str):
     t = Terminal()
     print(t.green(message))
 
 
-def print_json(data, colorize=False):
+def print_json(data: dict, colorize: bool=False):
     """
     Print a json payload.
     :param data: the json payload to print
@@ -37,13 +46,13 @@ def print_json(data, colorize=False):
         print(json_str)
 
 
-def datetime_from_utc_to_local(utc_datetime):
+def datetime_from_utc_to_local(utc_datetime: int):
     now_timestamp = time.time()
     offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
     return utc_datetime + offset
 
 
-def print_time(seconds):
+def print_time(seconds: int):
     sign_string = '-' if seconds < 0 else ''
     seconds = abs(int(seconds))
     days, seconds = divmod(seconds, 86400)
@@ -63,21 +72,36 @@ def get_nexus_client():
     key, cfg = get_selected_deployment_config()
     if cfg is None:
         error("You must select a profile.")
-    nxs.config.set_environment(cfg["url"])
-    nxs.config.set_token(cfg["token"])
+    nxs.config.set_environment(cfg[_URL_KEY_])
+    if _TOKEN_KEY_ in cfg:
+        nxs.config.set_token(cfg[_TOKEN_KEY_])
+    else:
+        warn("WARNING - you haven not set a token in your profile, use the 'auth set-token' command to do it.")
     return nxs
 
 
-def get_selected_deployment_base_url():
-    return get_selected_deployment_config()[1]['url'] + "/v0"
-
-
-def pretty_filesize(num, suffix='B'):
+def pretty_filesize(num: int , suffix: str='B'):
     for unit in ['','K','M','G','T','P','E','Z']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f %s%s" % (num, 'Yi', suffix)
+
+
+def remove_nexus_metadata(d: dict):
+    """ Returns a copy of the provided dictionary without nexus metadata (i.e. root keys starting with '_'). """
+    x = dict()
+    for k in d.keys():
+        if not k.startswith('_'):
+            x[k] = d[k]
+    return x
+
+
+def generate_nexus_payload_checksum(payload: dict):
+    """ Given a nexus payload, remove nexus metadata, order the keys and generate a MD5. """
+    filtered = remove_nexus_metadata(payload)
+    data_ordered = OrderedDict(sorted(filtered.items()))
+    return hashlib.md5(json.dumps(data_ordered, indent=2).encode('utf-8')).hexdigest()
 
 
 #######################
@@ -110,24 +134,14 @@ def get_cli_config():
     return data
 
 
-def save_cli_config(dict_cfg):
+def save_cli_config(dict_cfg: dict):
     """Save the given dictionary in the CLI config directory."""
     cfg_file = get_cli_config_file()
     with open(cfg_file, 'w') as fp:
         json.dump(dict_cfg, fp, sort_keys=True, indent=4)
 
 
-def get_access_token():
-    name, cfg = get_selected_deployment_config()
-    if cfg is None:
-        error("Failed to get selected config.")
-    token = None
-    if 'tokens' in cfg:
-        token = cfg['tokens']['access_token']
-    return token
-
-
-def get_selected_deployment_config(config=None):
+def get_selected_deployment_config(config: dict=None):
     """Searches for currently selected nexus profile.
        Returns a tuple containing (name, config) or None if not found.
     """
@@ -135,13 +149,9 @@ def get_selected_deployment_config(config=None):
         # load from disk if not given
         config = get_cli_config()
     for key in config.keys():
-        if 'selected' in config[key] and config[key]['selected'] is True:
+        if _SELECTED_KEY_ in config[key] and config[key][_SELECTED_KEY_] is True:
             return key, config[key]
     return None, None
-
-
-_DEFAULT_ORGANISATION_KEY_ = "default_organization"
-_DEFAULT_PROJECT_KEY_ = "default_project"
 
 
 def get_default_organization():
@@ -149,10 +159,13 @@ def get_default_organization():
     profile, selected_config = get_selected_deployment_config(config)
     if selected_config is None:
         error("You must first select a profile using the 'profiles' command")
-    return selected_config[_DEFAULT_ORGANISATION_KEY_]
+    if _DEFAULT_ORGANISATION_KEY_ in selected_config:
+        return selected_config[_DEFAULT_ORGANISATION_KEY_]
+    else:
+        return None
 
 
-def set_default_organization(org_label):
+def set_default_organization(org_label: str):
     config = get_cli_config()
     profile, selected_config = get_selected_deployment_config(config)
     if selected_config is None:
@@ -161,15 +174,27 @@ def set_default_organization(org_label):
     save_cli_config(config)
 
 
+def get_organization_label(given_org_label: str):
+    if given_org_label is None:
+        given_org_label = get_default_organization()
+        if given_org_label is None:
+            error("No organization specified, either set default using the 'orgs' command or pass it as a "
+                  "parameter using --org")
+    return given_org_label
+
+
 def get_default_project():
     config = get_cli_config()
     profile, selected_config = get_selected_deployment_config(config)
     if selected_config is None:
         error("You must first select a profile using the 'profiles' command")
-    return selected_config[_DEFAULT_PROJECT_KEY_]
+    if _DEFAULT_PROJECT_KEY_ in selected_config:
+        return selected_config[_DEFAULT_PROJECT_KEY_]
+    else:
+        return None
 
 
-def set_default_project(project_label):
+def set_default_project(project_label: str):
     config = get_cli_config()
     profile, selected_config = get_selected_deployment_config(config)
     if selected_config is None:
@@ -177,3 +202,11 @@ def set_default_project(project_label):
     config[profile][_DEFAULT_PROJECT_KEY_] = project_label
     save_cli_config(config)
 
+
+def get_project_label(given_project_label: str):
+    if given_project_label is None:
+        given_project_label = get_default_project()
+        if given_project_label is None:
+            error("No project specified, either set default using the 'projects' command or pass it as a "
+                  "parameter using --project")
+    return given_project_label
