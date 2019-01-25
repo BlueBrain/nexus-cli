6a1a4078ef3aef6030252c9dbf6fc21b5e71d76a
Samuel Kerrien <samuel.kerrien@epfl.ch>
2019-01-25 16:57:53 +0100

    first working version of 'acls make-public'

--- nexuscli/acls.py
+++ nexuscli/acls.py
@@ -96,51 +96,46 @@ def show_permissions(_json, pretty):
         utils.error(str(e))
 
 
-@acls.command(name='make-public', help='Make current project public')
+@acls.command(name='make-public', help='Make current project public (i.e. Add permission ''projects/read'' to ''Anonymous'')')
 @click.option('_org_label', '--org', '-o', help='Organization to work on (overrides selection made via orgs command)')
 @click.option('_prj_label', '--project', '-p', help='Project to work on (overrides selection made via projects command)')
-def make_public(_org_label, _prj_label):
+@click.option('_replace', '--replace-existing', is_flag=True, default=False,
+              help='If set, remove existing ACLs on this project and ')
+@click.option('_json', '--json', '-j', is_flag=True, default=False, help='Print JSON payload returned by the nexus API')
+@click.option('--pretty', '-p', is_flag=True, default=False, help='Colorize JSON output')
+def make_public(_org_label, _prj_label, _replace, _json, pretty):
     _org_label = utils.get_organization_label(_org_label)
     _prj_label = utils.get_project_label(_prj_label)
     nxs = utils.get_nexus_client()
     try:
         path = _org_label + '/' + _prj_label
+
+        utils.warn("You are about to make the data in your project public to the world.")
+        input("Press <ENTER> to continue, CTRL+C to cancel...")
+
         response = nxs.acls.fetch(subpath=path)
-        utils.print_json(response, colorize=True)
+        count = len(response["_results"])
+        if count > 1:
+            utils.error("More than one ACL matching: %d" % count)
+        elif count == 0:
+            utils.warn("No ACL matching")
+            current_rev = 0
+        else:
+            current_rev = response["_results"][0]['_rev']
 
-        # count = len(response["_results"])
-        # if count > 1:
-        #     utils.error("More than one ACL matching: %d" % count)
-        # elif count == 0:
-        #     utils.warn("No ACL matching")
-
-        # current_rev = response["_results"][0]['_rev']
         _identities = [_ANONYMOUS_]
         _permissions = [_PROJECTS_READ_]
-        payload = _payload(_permissions, _identities, "Append")
-        utils.print_json(payload, colorize=True)
-        nxs.acls.append(subpath=path, identities=_identities, permissions=_permissions, rev=1)
+        if _replace:
+            print("Replacing existing ACLs on %s" % path)
+            response = nxs.acls.replace(subpath=path, identities=_identities, permissions=[_permissions],
+                                        rev=current_rev)
+        else:
+            print("Adding existing ACLs on %s" % path)
+            response = nxs.acls.append(subpath=path, identities=_identities, permissions=[_permissions],
+                                       rev=current_rev)
+        if _json:
+            utils.print_json(response, colorize=pretty)
     except nxs.HTTPError as e:
         utils.print_json(e.response.json(), colorize=True)
         utils.error(str(e))
 
-
-def _payload(permissions: List[List[str]], identities: List[Dict], operation: str = None) -> Dict:
-    """Create an ACLs payload. ``permissions`` and ``identities`` have the same order.
-
-    :param permissions: List of list of permissions.
-    :param identities: List of identities to which the permissions apply.
-    :param operation: (optional) Corresponding operation: 'Append' or 'Subtract'.
-    :return: Payload of the ACLs.
-    """
-    payload = {
-        "acl": [
-            {
-                "permissions": x,
-                "identity": y,
-            } for x, y in zip(permissions, identities)
-        ]
-    }
-    if operation is not None:
-        payload["@type"] = operation
-    return payload
