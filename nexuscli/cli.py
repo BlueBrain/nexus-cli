3fd963e4217773fbde23365db515db7ba77f1c1c
Mohameth François SY <mohamethfrancois.sy@gmail.com>
2019-02-21 16:40:34 +0100

    Added helpers modules in nexuscli/cli.py (#18)

--- nexuscli/cli.py
+++ nexuscli/cli.py
@@ -10,5 +10,5 @@ def cli():
     pass
 
 
-from nexuscli import profiles, auth, orgs, projects, resources, views, schemas, acls
+from nexuscli import profiles, auth, orgs, projects, resources, views, schemas, acls, helpers
 
