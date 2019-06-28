6cc6ebc03b57efa47690950631a6cce904b4ca0e
Wojtek Wajerowicz <wojwaj@gmail.com>
2019-06-28 15:48:37 +0200

    Add storages.

--- nexuscli/cli.py
+++ nexuscli/cli.py
@@ -10,5 +10,5 @@ def cli():
     pass
 
 
-from nexuscli import profiles, auth, orgs, projects, resources, views, schemas, acls, helpers, resolvers
+from nexuscli import profiles, auth, orgs, projects, resources, views, schemas, acls, helpers, resolvers, storages
 
