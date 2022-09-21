c017d8cf8fe305bbc4ff9eaceb52b2dad54bae55
Simon Dumas <simon.dumas@epfl.ch>
2022-09-21 13:45:02 +0200

    Allow to filter projects when performing namespace migration

--- plugins/cli-migrate-ns/src/main/scala/ch/epfl/bluebrain/nexus/cli/migrate/ns/Intent.scala
+++ plugins/cli-migrate-ns/src/main/scala/ch/epfl/bluebrain/nexus/cli/migrate/ns/Intent.scala
@@ -1,11 +1,22 @@
 package ch.epfl.bluebrain.nexus.cli.migrate.ns
 
+import ch.epfl.bluebrain.nexus.cli.sdk.api.model.ProjectRef
+
 import java.time.Instant
 
 sealed trait Intent extends Product with Serializable
 
 object Intent {
 
-  case class MigrateNs(lastUpdate: Instant, pageSize: Int, concurrency: Int) extends Intent
+  final case class MigrateNs(lastUpdate: Instant,
+                             pageSize: Int,
+                             concurrency: Int,
+                             projects: List[ProjectRef]) extends Intent {
+    def filter(input: List[ProjectRef]): List[ProjectRef] =
+      if(projects.isEmpty)
+        input
+      else
+        input.intersect(projects)
+  }
 
 }
